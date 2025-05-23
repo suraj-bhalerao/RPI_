cf27e6da5549f92aa1b0ba6f538df77065fde75a
surajB2024 <surajbhalerao2024@gmail.com>
2025-05-23 12:23:00 +0530

    New changes commits and till here all functionality working well on windows

--- Auto Upload/auto_upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PYTHON_SCRIPT="/home/sharukh/CIAP/one.py"
-HOME_SSID="AEPL-R&D"
-CURRENT_SSID=$(iwgetid -r)
-if [ "$CURRENT_SSID" == "$HOME_SSID" ]; then
-    python3 "$PYTHON_SCRIPT"
-fi
